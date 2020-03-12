5d5c1c284de74cdf4e468c76aa02f49c1c0ccd8a
nequid <s1nc4pp@gmail.com>
2020-03-12 19:08:25 +0300

    i

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
   console.log('Get request fired')
